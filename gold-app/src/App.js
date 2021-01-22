189817728911b5e349a18aea30bef11b107a0646
riggja <soccerheadj@gmail.com>
2021-01-21 16:16:26 -0900

    add some code

--- gold-app/src/App.js
+++ gold-app/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./NavBar.js";
+import Homepage from "./Homepage.js";
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <NavBar />
+      <section className="App-header">
         <p>
           Hello Goldstream! This is the app.
         </p>
         <a
-          className="App-link"
           href="https://github.com/riggja/Goldstream-App"
           target="_blank"
         >
           Here is the github
         </a>
-      </header>
+      </section>
     </div>
   );
 }
---
+++ gold-app/src/NavBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class NavBar extends Component {
+
+    render() {
+        return(
+        <nav>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
+            <link rel="stylesheet" href="css/style.css"></link>
+            <div id="smallmenu" className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                   Menu
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a className="dropdown-item" href="index.html">Home</a>
+                    <a className="dropdown-item" href="subPage.html">Files</a>
+                    <a className="dropdown-item" href="subPage.html">Settings</a>
+                </div>
+            </div>
+            <div>
+              <ul id="menu">
+                <li>
+                    <img src="src/GSELogo.png" alt="Goldstream Engineering Logo"></img>
+                </li>
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="subPage.html">Files</a>
+                </li>
+                <li>
+                    <a href="subPage.html">Settings</a>
+                </li>
+              </ul>
+            </div>
+         </nav>
+        )
+    }
+}
+
+export default NavBar
