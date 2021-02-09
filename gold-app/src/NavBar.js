16bf0bfc7d8e1515a72c7fd52543f4ef0d18afd6
root <root@LAPTOP-I80D0Q75.localdomain>
2021-02-08 23:38:40 -0900

    Add new colors

--- gold-app/src/NavBar.js
+++ gold-app/src/NavBar.js
@@ -11,17 +11,17 @@ class NavBar extends Component {
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
             <link rel="stylesheet" href="css/style.css"></link>
             <DropdownButton id="smallmenu" title="Menu">
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item href="/Homepage">Homepage</Dropdown.Item>
+                <Dropdown.Item href="/Files">Files</Dropdown.Item>
+                <Dropdown.Item href="/Settings">Settings</Dropdown.Item>
             </DropdownButton>
             <div>
               <ul id="menu">
                 <li>
-                    <img src="src/GSELogo.png" alt="Goldstream Engineering Logo"></img>
+                    <img src="src/Mammoth.jpg" alt="Goldstream Engineering Logo"></img>
                 </li>
                 <li>
-                <a href="index.html">Homepage</a>
+                    <a href="index.html">Homepage</a>
                 </li>
                 <li>
                     <a href="subPage.html">Files</a>
