32c11dac5ec116fe0dc88ab523f6853ac7ba3735
root <root@LAPTOP-I80D0Q75.localdomain>
2021-03-02 00:29:32 -0900

    add basic input

--- gold-app/src/pages/newFile.js
+++ gold-app/src/pages/newFile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import '../css/style.css';
 
 class newFile extends Component {
@@ -10,7 +12,7 @@ class newFile extends Component {
             <main>
                <section >
                     <h1>
-                        Files:
+                        Input Form:
                     </h1>
                     <Form>
                         <Form.Group controlId="JobNumber">
@@ -29,17 +31,41 @@ class newFile extends Component {
                             <Form.Label>Time: </Form.Label>
                             <Form.Control placeholder="Enter Time" />
                         </Form.Group>
-                        <Form.Group id="formGridCheckbox">
-                            <Form.Check type="checkbox" label="AM" />
-                            <Form.Check type="checkbox" label="PM" />
+                        <Form.Group as={Row}>
+                            <Col sm={10}>
+                                <Form.Check
+                                type="radio"
+                                label="AM"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios1"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="PM"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                                />
+                            </Col>
                         </Form.Group>
                         <Form.Group controlId="Temp">
                             <Form.Label>Temp: </Form.Label>
                             <Form.Control placeholder="Enter Temp" />
                         </Form.Group>
-                        <Form.Group id="formGridCheckbox">
-                            <Form.Check type="checkbox" label="F" />
-                            <Form.Check type="checkbox" label="C" />
+                        <Form.Group as={Row}>
+                            <Col sm={10}>
+                                <Form.Check
+                                type="radio"
+                                label="F"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios1"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="C"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                                />
+                            </Col>
                         </Form.Group>
                         <Form.Group controlId="Contractor">
                             <Form.Label>Contractor: </Form.Label>
@@ -56,6 +82,35 @@ class newFile extends Component {
                         <Form.Group controlId="Others">
                             <Form.Label>Others Present at Site: </Form.Label>
                             <Form.Control placeholder="Enter Others" />
+                        </Form.Group>
+                        <Form.Group as={Row}>
+                        <Form.Label as="legend" column sm={2}>
+                            Copies to: 
+                        </Form.Label>
+                            <Col sm={10}>
+                                <Form.Check
+                                type="radio"
+                                label="Called"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios1"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="Checked In"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                                />
+                                <Form.Check
+                                type="radio"
+                                label="E-mail"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios3"
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group controlId="Name">
+                            <Form.Label>Name: </Form.Label>
+                            <Form.Control placeholder="Enter Name" />
                         </Form.Group>
                     </Form>
                     <Button variant="outline-success">Add New File</Button>{' '}
